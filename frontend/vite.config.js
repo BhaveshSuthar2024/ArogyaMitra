0c6cca3a3b66a44d32200bf2b764de0538e8d7e3
Bhavesh Suthar <bsuthar2024@gmail.com>
2025-08-19 18:48:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Make sure paths work correctly on Render
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
